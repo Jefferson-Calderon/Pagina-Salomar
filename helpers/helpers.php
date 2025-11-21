628796e76b9498a8a9a8e861d6c82a05713a2707
Jefry-21 <jeffersoncalderonburgos53@gmail.com>
2025-11-21 01:13:43 -0500

    Version1

---
+++ helpers/helpers.php
@@ -0,0 +1,19 @@
+<?php
+
+// Retorna la URL del proyecto
+function base_url() {
+    return "http://localhost/Pagina_Web/";
+}
+
+// Retorna la URL de la carpeta public
+function media() {
+    return "http://localhost/Pagina_Web/assets";
+}
+
+// Retorna el nombre de la institución
+function name_D() {
+    return 'SANLOMAR GROUP S.A.C.';
+}
+function name() {
+    return 'Sanlomar Group S.A.C.';
+}
